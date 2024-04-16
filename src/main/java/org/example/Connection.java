cfcefc9109ce87ec3bef1113b4c988bfa1eca62a
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2024-04-16 13:53:37 +0200

    Start structure. 1.0

--- src/main/java/org/example/Connection.java
+++ src/main/java/org/example/Connection.java
@@ -7,11 +7,13 @@ import org.bson.Document;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.MongoClients.create;
+
 public class Connection {
 
     public static void main(String[] args) {
-        String connectionString = System.getProperty("mongodb.uri");
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+        String connectionString = System.getProperty("mongodb+srv://oleksiy_khudoshyn:Igibu777@cluster0.or7u1dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        try (MongoClient mongoClient = create(connectionString)) {
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
         }
